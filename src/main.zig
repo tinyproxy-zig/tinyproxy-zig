3892f71d4ed83ef51fa105a60655c78d898ff673
Dacheng Gao <successgdc@gmail.com>
2025-05-30 22:20:10 +0800

    use const config

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,11 @@ const child = @import("child.zig");
 
 const log = std.log.scoped(.tinyproxy);
 
+const LISTEN_ADDR: []const u8 = "127.0.0.1";
+const LISTEN_PORT: u16 = 9999;
+
 pub fn main() !void {
-    log.info("listening on 127.0.0.1:9900", .{});
-    try child.listen_sockets("127.0.0.1", 9900);
+    log.info("listening on {s}:{}", .{LISTEN_ADDR, LISTEN_PORT});
+    try child.listen_sockets(LISTEN_ADDR, LISTEN_PORT);
     try child.main_loop();
 }
